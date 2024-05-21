15c4284d79326461e1c78746018b41974b7128de
DefenderN <57073766+DefenderN@users.noreply.github.com>
2024-05-21 23:30:00 +0200

    Finished 5.1: Blog List Frontend, step 1
    - Cloned repo, deleted .git config, then published as new repo on github
    
    - npm install
    - Added loginForm
    - Added login functionality

---
+++ src/services/login.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+const baseUrl = '/api/login'
+
+const login = async (username, password) => {
+    const loginResponse = await axios.post(baseUrl, {username, password})
+    return loginResponse.data
+
+}   
+
+export default { login }
+
+
+
